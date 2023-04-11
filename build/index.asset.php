59ffc8aa7c7e9522e515c9a2e16874d2b9b1fa1a
Tariq <mmtariqk@gmail.com>
2023-04-11 17:10:10 +0500

    Init

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '9418102b56e49aab550e');
---
+++ build/render.php
@@ -0,0 +1,3 @@
+<p <?php echo get_block_wrapper_attributes(); ?>>
+	<?php esc_html_e( 'Tariq Block Poll – hello from a dynamic block!', 'tariq-poll' ); ?>
+</p>
